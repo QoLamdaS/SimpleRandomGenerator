ebcee6c2e7d7304122b7468a4a36d3f475e4838a
WillyS <kaffeeunused@gmail.com>
2024-06-15 20:55:08 +0700

    Make a function to randomize number and printthe randomized number from the Function

--- Simple Random Generator.py
+++ Simple Random Generator.py
@@ -1,1 +1,11 @@
 import random
+
+def FullRandom():
+    a = random.randint(3469, 4357)
+    b = random.randint(454, 9585)
+    EarlyTotal = a * b * a * a
+    c = random.randint(26, 82)
+    FinalTotal = EarlyTotal / c
+    return FinalTotal
+
+print(f"The Number: {FullRandom()}")
