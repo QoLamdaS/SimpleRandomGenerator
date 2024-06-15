78ddd75418708d67b7dc08908426a4a6940a275d
WillyS <kaffeeunused@gmail.com>
2024-06-15 20:40:09 +0700

    delete the whole things and import random library

--- Simple Random Generator.py
+++ Simple Random Generator.py
@@ -1,9 +1,1 @@
-import string
-
-AlphabetUppercase = []
-AlphabetUppercase = list(string.ascii_uppercase)
-alphabet_lowercase = []
-alphabet_lowercase = list(string.ascii_lowercase)
-Punctuation = []
-Punctuation = list(string.punctuation)
-
+import random
