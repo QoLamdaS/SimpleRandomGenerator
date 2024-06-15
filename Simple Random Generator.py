91b44e38f07a14f3f8087258a6fca9dd1dc7fdfb
WillyS <kaffeeunused@gmail.com>
2024-06-15 19:58:02 +0700

    Declare variables lists

---
+++ Simple Random Generator.py
@@ -0,0 +1,9 @@
+import string
+
+AlphabetUppercase = []
+AlphabetUppercase = list(string.ascii_uppercase)
+alphabet_lowercase = []
+alphabet_lowercase = list(string.ascii_lowercase)
+Punctuation = []
+Punctuation = list(string.punctuation)
+
